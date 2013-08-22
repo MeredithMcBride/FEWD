8c78418864998d217d60197cb90a2863785a1d14
MeredithMcBride <Meredith.j.mcbride@gmail.com>
2013-08-22 22:24:53 +0800

    Lesson 12 conditionals

--- lesson12/labs/coaConditionals/conditionals.js
+++ lesson12/labs/coaConditionals/conditionals.js
@@ -5,24 +5,48 @@
 
 	 //Prompt the user for their name and last name. 
 
+	 var firstname = prompt('What is your first name?');
+	 var surname = prompt('What is your surname?');
 
 	//Create a new variable called full name and store the users full name.
 	
-	
+	var fullname = store ()
 	//Print the full name to the console.
 	
-
+	console.log(fullname);
 	//Prompt the user for their age.
 	
 
-
+	var age = prompt('What is your age?');
 	//Add 10 to the age and print the output to the console.
 	
-	
+	age = parseInt(age) + 10;
+
+	age = age + 10;
 	
 	//Verify that the user is over 18 and print if they are a minor or adult to the console.
+	
+	var age = ('18');
+
+	If age == ('18') {
+		Console.log('come on in');
+	} else if age == greater than('18') {
+		console.log('come on in');
+	} else {
+		console.log('see ya');
+	}
+
 
 
 	
 	//Verify if the first name is "General" and the last name is NOT "Assembly"
+	var first name = ('General');
+
+	If first name == ('General') {
+		Console.log('come on in');
+	} else if age == greater than('18') {
+		console.log('come on in');
+	} else {
+		console.log('see ya');
+	}
 
